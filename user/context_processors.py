e771f2c894eea0cb22c926ed4dfb0f8ada4ea390
kirisame19 <kirisamekari19@gmail.com>
2025-02-05 12:21:43 +0900

    コメント修正

--- user/context_processors.py
+++ user/context_processors.py
@@ -3,5 +3,5 @@ from django.conf import settings
 # 全体のテンプレートが使える変数
 def constant_data(request):
     return {
-        "APP_TITLE": settings.APP_TITLE,
+        "APP_TITLE": settings.APP_TITLE,    # アプリのタイトルを全テンプレートで使用可能にする
     }
